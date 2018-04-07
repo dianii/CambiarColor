ff289c31c5de2ba62120eeac3c0926b864ae6ab3
dianii <38058753+dianii@users.noreply.github.com>
2018-04-07 14:28:46 -0500

    BoxView

--- CambiarColor/CambiarColor/CambiarColor/MainPage.xaml.cs
+++ CambiarColor/CambiarColor/CambiarColor/MainPage.xaml.cs
@@ -7,11 +7,31 @@ using Xamarin.Forms;
 
 namespace CambiarColor
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        private void ValueChanged(object sender, EventArgs e) {
+            var rojo = this.sldRojo.Value;
+            var verde = this.sldRojo.Value;
+            var azul = this.sldAzul.Value;
+            var opacidad = this.sldOpacidad.Value;
+            Color bgColor = new Color(rojo, verde, azul, opacidad);
+            boxColor.BackgroundColor = bgColor;
+            lbDisplay.Text = colorHexa(bgColor);
+
+        }
+
+        private string colorHexa(Color bgColor)
+        {
+            int rojo = (int)(bgColor.R * 255);
+            int verde= (int)(bgColor.G * 255);
+            int azul = (int)(bgColor.B * 255);
+            int opacidad = (int)(bgColor.A * 255);
+            return$"# {rojo:x2}{verde:x2}{azul:x2}{opacidad:x2}";
+        }
+    }
 }
